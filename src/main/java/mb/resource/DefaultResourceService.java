5f979162404b0dc2f28df790698a248ce8c7c82e
Gabriel Konat <gabrielkonat@gmail.com>
2019-08-02 15:55:28 +0200

    Add overloads for all resource types.

--- src/main/java/mb/resource/DefaultResourceService.java
+++ src/main/java/mb/resource/DefaultResourceService.java
@@ -1,6 +1,7 @@
 package mb.resource;
 
 import mb.resource.hierarchical.HierarchicalResource;
+import mb.resource.hierarchical.ResourcePath;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.HashMap;
@@ -41,6 +42,30 @@ public class DefaultResourceService implements ResourceService {
         return registry.getResource(key.getId());
     }
 
+    @Override public ReadableResource getReadableResource(ResourceKey key) {
+        final Resource resource = getResource(key);
+        if(!(resource instanceof ReadableResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a readable resource");
+        }
+        return (ReadableResource) resource;
+    }
+
+    @Override public WritableResource getWritableResource(ResourceKey key) {
+        final Resource resource = getResource(key);
+        if(!(resource instanceof WritableResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a writable resource");
+        }
+        return (WritableResource) resource;
+    }
+
+    @Override public HierarchicalResource getHierarchicalResource(ResourcePath path) {
+        final Resource resource = getResource(path);
+        if(!(resource instanceof HierarchicalResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a hierarchical resource");
+        }
+        return (HierarchicalResource) resource;
+    }
+
 
     @Override public ResourceKey getResourceKey(String keyStr) {
         final ResourceKeyConverter.@Nullable Parsed parsedResourceKey = ResourceKeyConverter.parse(keyStr);
@@ -57,6 +82,15 @@ public class DefaultResourceService implements ResourceService {
         return registry.getResourceKey(parsedResourceKey.idStr);
     }
 
+    @Override public ResourcePath getResourcePath(String pathStr) {
+        final ResourceKey key = getResourceKey(pathStr);
+        if(!(key instanceof ResourcePath)) {
+            throw new ResourceRuntimeException("Resource key '" + key + "' is not a path");
+        }
+        return (ResourcePath) key;
+    }
+
+
     @Override public Resource getResource(String keyStr) {
         final ResourceKeyConverter.@Nullable Parsed parsedResourceKey = ResourceKeyConverter.parse(keyStr);
         if(parsedResourceKey == null) {
@@ -72,6 +106,31 @@ public class DefaultResourceService implements ResourceService {
         return registry.getResource(parsedResourceKey.idStr);
     }
 
+    @Override public ReadableResource getReadableResource(String keyStr) {
+        final Resource resource = getResource(keyStr);
+        if(!(resource instanceof ReadableResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a readable resource");
+        }
+        return (ReadableResource) resource;
+    }
+
+    @Override public WritableResource getWritableResource(String keyStr) {
+        final Resource resource = getResource(keyStr);
+        if(!(resource instanceof WritableResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a writable resource");
+        }
+        return (WritableResource) resource;
+    }
+
+    @Override public HierarchicalResource getHierarchicalResource(String pathStr) {
+        final Resource resource = getResource(pathStr);
+        if(!(resource instanceof HierarchicalResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a hierarchical resource");
+        }
+        return (HierarchicalResource) resource;
+    }
+
+
     @Override public Resource appendOrReplaceWith(HierarchicalResource resource, String keyStrOrPath) {
         final ResourceKeyConverter.@Nullable Parsed parsedResourceKey = ResourceKeyConverter.parse(keyStrOrPath);
         if(parsedResourceKey != null) {
@@ -84,6 +143,16 @@ public class DefaultResourceService implements ResourceService {
         }
         return resource.appendOrReplaceWithPath(keyStrOrPath);
     }
+
+    @Override
+    public HierarchicalResource appendOrReplaceWithHierarchical(HierarchicalResource resource, String pathStrOrPath) {
+        final Resource newResource = appendOrReplaceWith(resource, pathStrOrPath);
+        if(!(newResource instanceof HierarchicalResource)) {
+            throw new ResourceRuntimeException("Resource '" + resource + "' is not a hierarchical resource");
+        }
+        return (HierarchicalResource) newResource;
+    }
+
 
     @Override public String toStringRepresentation(ResourceKey key) {
         final String qualifier = key.getQualifier();
--- src/main/java/mb/resource/ResourceService.java
+++ src/main/java/mb/resource/ResourceService.java
@@ -1,6 +1,7 @@
 package mb.resource;
 
 import mb.resource.hierarchical.HierarchicalResource;
+import mb.resource.hierarchical.ResourcePath;
 
 public interface ResourceService {
     /**
@@ -15,11 +16,53 @@ public interface ResourceService {
      */
     Resource getResource(ResourceKey key);
 
+    /**
+     * Gets {@link ReadableResource readable resource} for given {@link ResourceKey resource key}.
+     *
+     * @param key {@link ResourceKey Key} to get readable resource for.
+     * @return Readable resource.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourceKey#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link ReadableResource readable
+     *                                  resource}.
+     */
+    ReadableResource getReadableResource(ResourceKey key);
+
+    /**
+     * Gets {@link WritableResource writable resource} for given {@link ResourceKey resource key}.
+     *
+     * @param key {@link ResourceKey Key} to get writable resource for.
+     * @return Writable resource.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourceKey#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link WritableResource writable
+     *                                  resource}.
+     */
+    WritableResource getWritableResource(ResourceKey key);
+
+    /**
+     * Gets {@link HierarchicalResource hierarchical resource} for given {@link ResourcePath resource path}.
+     *
+     * @param path {@link ResourcePath Path} to get hierarchical resource for.
+     * @return Hierarchical resource.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourcePath#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link HierarchicalResource
+     *                                  hierarchical resource}.
+     */
+    HierarchicalResource getHierarchicalResource(ResourcePath path);
+
 
     /**
      * Gets {@link ResourceKey resource key} for given string representation of the resource key.
      *
-     * @param keyStr String representation of the {@link ResourceKey resource key}.
+     * @param keyStr String representation of the resource key.
      * @return Resource key.
      * @throws ResourceRuntimeException when {@code keyStr} is not a valid string representation of a {@link ResourceKey
      *                                  resource key}.
@@ -27,6 +70,21 @@ public interface ResourceService {
      *                                  ResourceKey#getQualifier() qualifier}.
      */
     ResourceKey getResourceKey(String keyStr);
+
+    /**
+     * Gets {@link ResourcePath resource path} for given string representation of the resource path.
+     *
+     * @param pathStr String representation of the resource path.
+     * @return Resource path.
+     * @throws ResourceRuntimeException when {@code pathStr} is not a valid string representation of a {@link
+     *                                  ResourcePath resource path}.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourcePath#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when a resource key could be created, but it is not a {@link ResourcePath
+     *                                  resource path}.
+     */
+    ResourcePath getResourcePath(String pathStr);
+
 
     /**
      * Gets {@link Resource resource} for given string representation of a {@link ResourceKey resource key}.
@@ -43,6 +101,58 @@ public interface ResourceService {
     Resource getResource(String keyStr);
 
     /**
+     * Gets a {@link ReadableResource readable resource} for given string representation of a {@link ResourceKey
+     * resource key}.
+     *
+     * @param keyStr String representation of a resource key.
+     * @return Readable resource.
+     * @throws ResourceRuntimeException when {@code keyStr} is not a valid string representation of a {@link ResourceKey
+     *                                  resource key}.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourceKey#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link ReadableResource readable
+     *                                  resource}.
+     */
+    ReadableResource getReadableResource(String keyStr);
+
+    /**
+     * Gets a {@link WritableResource writable resource} for given string representation of a {@link ResourceKey
+     * resource key}.
+     *
+     * @param keyStr String representation of a resource key.
+     * @return Writable resource.
+     * @throws ResourceRuntimeException when {@code keyStr} is not a valid string representation of a {@link ResourceKey
+     *                                  resource key}.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourceKey#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link WritableResource writable
+     *                                  resource}.
+     */
+    WritableResource getWritableResource(String keyStr);
+
+    /**
+     * Gets a {@link HierarchicalResource hierarchical resource} for given string representation of a {@link
+     * ResourcePath resource path}.
+     *
+     * @param pathStr String representation of a resource path
+     * @return Hierarchical resource.
+     * @throws ResourceRuntimeException when {@code pathStr} is not a valid string representation of a {@link
+     *                                  ResourcePath resource path}.
+     * @throws ResourceRuntimeException when no {@link ResourceRegistry resource registry} is found for {@link
+     *                                  ResourcePath#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when the {@link ResourceRegistry resource registry} unexpectedly fails to get
+     *                                  the resource.
+     * @throws ResourceRuntimeException when a resource is found, but it is not a {@link HierarchicalResource
+     *                                  hierarchical resource}.
+     */
+    HierarchicalResource getHierarchicalResource(String pathStr);
+
+
+    /**
      * If {@code pathOrKey} is a string representation of a {@link ResourceKey resource key}, returns a (replaced)
      * {@link Resource resource} for that key. Otherwise, returns a resource where given {@code pathOrKey} is appended
      * (or replaced with) as a path to given {@code resource} using {@link HierarchicalResource#appendOrReplaceWithPath(String)}.
@@ -50,7 +160,7 @@ public interface ResourceService {
      * @param resource     Resource to attempt to append {@code pathOrKey} to.
      * @param keyStrOrPath String representation of a {@link ResourceKey key}, or path which should be appended to
      *                     {@code resource}.
-     * @return Resource result.
+     * @return Resource.
      * @throws ResourceRuntimeException when {@code keyStrOrPath} is a key and no {@link ResourceRegistry resource
      *                                  registry} is found for {@link ResourceKey#getQualifier() qualifier}.
      * @throws ResourceRuntimeException when {@code keyStrOrPath} is a key and the {@link ResourceRegistry resource
@@ -59,6 +169,28 @@ public interface ResourceService {
      *                                  fails unexpectedly.
      */
     Resource appendOrReplaceWith(HierarchicalResource resource, String keyStrOrPath);
+
+    /**
+     * If {@code pathOrKey} is a string representation of a {@link ResourcePath resource path}, returns a (replaced)
+     * {@link HierarchicalResource hierarchical resource} for that path. Otherwise, returns a resource where given
+     * {@code pathOrKey} is appended (or replaced with) as a path to given {@code resource} using {@link
+     * HierarchicalResource#appendOrReplaceWithPath(String)}.
+     *
+     * @param resource      Resource to attempt to append {@code pathOrKey} to.
+     * @param pathStrOrPath String representation of a {@link ResourcePath resource path}, or path which should be
+     *                      appended to {@code resource}.
+     * @return Hierarchical resource.
+     * @throws ResourceRuntimeException when {@code pathStrOrPath} is a resource path and no {@link ResourceRegistry
+     *                                  resource registry} is found for {@link ResourceKey#getQualifier() qualifier}.
+     * @throws ResourceRuntimeException when {@code keyStrOrPath} is a resource path and the {@link ResourceRegistry
+     *                                  resource registry} unexpectedly fails to get the resource.
+     * @throws ResourceRuntimeException when {@code keyStrOrPath} is a resource path and a resource is found, but it is
+     *                                  not a {@link HierarchicalResource hierarchical resource}.
+     * @throws ResourceRuntimeException when {@code keyStrOrPath} is a path and appending the path to {@code resource}
+     *                                  fails unexpectedly.
+     */
+    HierarchicalResource appendOrReplaceWithHierarchical(HierarchicalResource resource, String pathStrOrPath);
+
 
     /**
      * Converts given {@link ResourceKey resource key} into its string representation.
