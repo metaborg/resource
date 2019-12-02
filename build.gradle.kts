431ebc23a6281c612b5039d6314f7a4fdff2392f
Gabriel Konat <gabrielkonat@gmail.com>
2019-12-02 11:26:31 +0100

    Bump plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.10"
-  id("org.metaborg.gradle.config.java-library") version "0.3.10"
-  id("org.metaborg.gradle.config.junit-testing") version "0.3.10"
+  id("org.metaborg.gradle.config.root-project") version "0.3.12"
+  id("org.metaborg.gradle.config.java-library") version "0.3.12"
+  id("org.metaborg.gradle.config.junit-testing") version "0.3.12"
   id("org.metaborg.gitonium") version "0.1.2"
 }
 
