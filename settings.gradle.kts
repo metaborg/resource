fc0f8db0628d1144b622b570efc6729f3c91c4e3
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-04-16 13:26:55 +0200

    Remove support for Gradle < 6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@ pluginManagement {
   }
 }
 
-if(org.gradle.util.VersionNumber.parse(gradle.gradleVersion).major < 6) {
-  enableFeaturePreview("GRADLE_METADATA")
-}
 
 fun includeProject(path: String, id: String = "resource.${path.replace('/', '.')}") {
   include(id)
