86e14f6b82dd85771cc82ce0f7c4fd2e261ee86a
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 18:01:46 +0200

    Use metaborg convention settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,16 @@
 rootProject.name = "resource.root"
 
-// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
     }
 }
 
-// This allows us to use the catalog in dependencies
-dependencyResolutionManagement {
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-    }
-    versionCatalogs {
-        create("libs") {
-            from("org.metaborg.spoofax3:catalog:0.3.3")
-        }
-    }
+plugins {
+    id("org.metaborg.convention.settings") version "0.0.6"
 }
 
-// This downloads an appropriate JVM if not already available
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+
 
 
 fun includeProject(path: String, id: String = "resource.${path.replace('/', '.')}") {
