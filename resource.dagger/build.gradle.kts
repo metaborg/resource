f5819264529f715e90ea5058354501230d37e3bc
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-18 16:03:34 +0200

    Use Metaborg convention/platform latest snapshot

--- resource.dagger/build.gradle.kts
+++ resource.dagger/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 group = "org.metaborg"
 
 dependencies {
-    api(platform(libs.metaborg.platform))
+    api(platform(libs.metaborg.platform)) { version { require("latest.integration") } }
 
     api(project(":resource"))
     api(libs.metaborg.log.dagger)
